915ad84d875ce58e53f9e0d408bdb33d8b264651
redtrib3 <anirudhkp2004@gmail.com>
2024-05-18 00:02:04 +0530

    bug fix

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(redirectSubdomain);
+app.use(RedirectSubdomain);
 app.use(redirectRoutes);
 app.use('/api', apiRoutes);
 
